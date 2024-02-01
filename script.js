18df231dee866bf40e696810002c2d1b1edcb0aa
Henrique <henrynyyls@gmail.com>
2024-02-01 08:48:04 -0300

    Added documentation for easier reading of the code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var selectedRow = null;
 
-// Show alerts
+// Mostra alertas
 function showAlert(message,className){
     const div = document.createElement("div");
     div.className = `alert alert-${className}`;
@@ -8,36 +8,40 @@ function showAlert(message,className){
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector(".container");
     const main = document.querySelector(".main");
-
+    //Cria Alerta
     container.insertBefore(div,main);
 
+    // Tempo até sumir o alerta
     setTimeout(() => document.querySelector(".alert").remove(),3000);
 }
 
-//Clear All Fields
+//Limpa todos os campos
 function clearFields (){
     document.querySelector("#firstName").value = "";
     document.querySelector("#lastName").value = "";
     document.querySelector("#rollNo").value = "";
 }
 
-//Add Data
+//Adiciona informações
 document.querySelector("#student-form").addEventListener("submit",(e)=> {
     e.preventDefault();
 
-    // Get Form Values
+    // Pega os valores do formulário
     const firstName = document.querySelector("#firstName").value;
     const lastName = document.querySelector("#lastName").value;
     const rollNo = document.querySelector("#rollNo").value;
 
-    //Validate
+    //Valida os valores
     if(firstName == "" || lastName == "" || rollNo == ""){
+        // Alerta caso algum esteja vazio
         showAlert("Please fill in all fields", "danger");
     }else{
+        // Cria linha com as informações obtidas
         if(selectedRow == null){
             const list = document.querySelector("#student-list");
             const row = document.createElement("tr");
 
+            // Cria as linhas E os botões para editar e deletar
             row.innerHTML = `
                 <td>${firstName}</td>
                 <td>${lastName}</td>
@@ -63,10 +67,11 @@ document.querySelector("#student-form").addEventListener("submit",(e)=> {
 
 });
 
-//Edit Data
+//Edita Informações
 document.querySelector("#student-list").addEventListener("click",(e)=> {
     target = e.target;
     if(target.classList.contains("edit")){
+        // Seleciona as informações da linha selecionada(da lista)
         selectedRow = target.parentElement.parentElement;
         document.querySelector("#firstName").value = selectedRow.children[0].textContent;
         document.querySelector("#lastName").value = selectedRow.children[1].textContent;
@@ -74,10 +79,11 @@ document.querySelector("#student-list").addEventListener("click",(e)=> {
     }
 });
 
-//Delete Data
+//Remove Informações
 document.querySelector("#student-list").addEventListener("click",(e) => {
     target = e.target;
     if(target.classList.contains("delete")){
+        // Remove as informações sobre a linha
         target.parentElement.parentElement.remove();
         showAlert("Student Data Deleted", "danger");
     }
